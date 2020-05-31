cd995fea930239957b4df628c87aa16a75dff4fb
lalithaditya20 <sowjanyalalith@gmail.com>
2020-05-31 21:31:39 +0530

    my first commit

---
+++ sumf.py
@@ -0,0 +1,8 @@
+def sum(x,y):
+    return x+y
+
+a=int(input("enter num 1= "))
+b=int(input("enter num 2= "))
+c=sum(a,b)
+
+print("sum of",a,"and",b,"is",c)
---
+++ tables.py
@@ -0,0 +1,800 @@
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+    
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+    
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+    
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+n=int(input("enter a number= "))
+for i in range (1,21):
+    print(n,"*",i,"=",n*i)
+
+
+
+
+
+
