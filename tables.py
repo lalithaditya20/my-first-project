b96d477581cc057e305bc0e77bf84eefa4c68056
jisasbo <satishbabu.balusu@jbhunt.com>
2020-06-02 21:26:33 -0500

    updated tables program

--- tables.py
+++ tables.py
@@ -1,10 +1,10 @@
 while True:
 	n=int(input("enter a number between 1 and 1000 inclusive..."))
 	if(n <= 0):
-		break
-	elif
-		for i in range (1,21):
-			print (n,"*",i,"=",n*i)
+	   break
+	else:
+	   for i in range (1,21):
+	      print (n,"*",i,"=",n*i)
 
                   
 
