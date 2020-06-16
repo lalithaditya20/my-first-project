6159aed684760302b9e0133045f0eae5fec70dd1
lalithaditya20 <sowjanyalalith@gmail.com>
2020-06-16 08:13:06 +0530

    my fifth commit

---
+++ Age.py
@@ -0,0 +1,18 @@
+n=int(input("enter your age... " ))
+if(n==1):
+    print("infant")
+elif(n>=2 and n<=6):
+    print("child")
+elif(n>=7 and n<=20):
+    print("young")
+elif (n>=21 and n<=45):
+    print("middle aged")
+else:
+    print("old")
+
+
+
+
+
+
+
