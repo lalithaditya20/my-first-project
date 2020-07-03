419f50f4ba234f690130d92b8d5e125356368b20
lalithaditya20 <sowjanyalalith@gmail.com>
2020-07-03 08:57:48 +0530

    my fifth commit

---
+++ loop4.py
@@ -0,0 +1,12 @@
+#This program takes the range and a number as input and prints all the numbers divisible by the given number in the given range.
+
+n = int(input("enter the number... "))
+x = int(input("enter the range... "))
+y = n
+while y <= x:
+    # if (y % n == 0):
+    print(y)
+    y = y + n
+
+
+
