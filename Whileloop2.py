deabe0ae5c73cad1241413455b892f67b54c9c3f
lalithaditya20 <sowjanyalalith@gmail.com>
2020-07-05 09:16:57 +0530

    every file was added to git

---
+++ Whileloop2.py
@@ -0,0 +1,16 @@
+# i = 2
+# n = int(input("enter the range... "))
+#
+#
+# while i <= n:
+#     # print(i)
+#     # i = i + 2
+# # if i==1:
+# #     print(i)
+
+i = 1
+n = int(input("enter the range... "))
+while i <= n:
+    if (i % 2 == 0):
+        print(i)
+    i += 1
