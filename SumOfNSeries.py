b764ea8dbb2b2e5a1f4ee1a7b8de1c26fc57dde0
lalithaditya20 <sowjanyalalith@gmail.com>
2020-07-05 09:13:58 +0530

    did this yesterday

---
+++ SumOfNSeries.py
@@ -0,0 +1,9 @@
+# this program will take a number as input and prints the sum of numbers from 1 to the given number.
+n = int(input("enter the number "))
+sum = 0
+i = 1
+while i<=n:
+    sum += i #sum = sum + i
+    i += 1
+print(sum)
+
